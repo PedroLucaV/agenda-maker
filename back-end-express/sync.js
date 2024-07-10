d569a0622379a191e01310fe9fa4b0fd05bbfa97
Pedro <izielucas2016@gmail.com>
2024-07-10 20:22:46 -0300

    sync database code

---
+++ back-end-express/sync.js
@@ -0,0 +1,14 @@
+import sequelize from './controller/dbconfig.js'
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected!');
+    await sequelize.sync({ force: true }); //para remover ao lancar a aplicação o force:true
+    console.log('Database synced!');
+  } catch (error) {
+    console.error('Error syncing database:', error);
+  } finally {
+    await sequelize.close();
+  }
+})();
