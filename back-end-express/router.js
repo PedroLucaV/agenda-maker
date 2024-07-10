beeeacdc7453d058f6c03138750675b50bbcae89
Pedro <izielucas2016@gmail.com>
2024-07-10 20:24:52 -0300

    added first test route to code

--- back-end-express/router.js
+++ back-end-express/router.js
@@ -1,4 +1,16 @@
 import { Router } from "express";
+import User from './models/User.js';
 const router = Router();
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.findAll({
+      attributes: ['nome', 'senha']
+    });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 export default router;
