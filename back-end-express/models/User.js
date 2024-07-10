653c1caec9ed07d8c27ff3ccc8cc972036287b90
Pedro <izielucas2016@gmail.com>
2024-07-10 20:23:32 -0300

    user model created

---
+++ back-end-express/models/User.js
@@ -0,0 +1,52 @@
+import { DataTypes } from '@sequelize/core';
+import sequelize from '../controller/dbconfig.js';
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.CHAR(40),
+    primaryKey: true,
+    allowNull: false
+  },
+  nome: {
+    type: DataTypes.STRING(200),
+    allowNull: false
+  },
+  foto_usuario: {
+    type: DataTypes.STRING(1500),
+    allowNull: true
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true
+  },
+  senha: {
+    type: DataTypes.CHAR(60),
+    allowNull: false
+  },
+  is_admin: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: 0
+  },
+  telefone: {
+    type: DataTypes.STRING(20),
+    allowNull: true
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+export default User;
