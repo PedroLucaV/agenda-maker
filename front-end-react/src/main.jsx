d9f9fd025bdf65643282609a5ef5957e4e949ac0
lucarl07 <junitinho2007@gmail.com>
2024-07-17 12:18:15 -0300

    :bulb: docs: Describing imports at src/main.jsx

--- front-end-react/src/main.jsx
+++ front-end-react/src/main.jsx
@@ -1,6 +1,10 @@
+// Dependencies:
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/App/App"
+
+// Files & Pages:
+import App from "./pages/App/App";
+import Login from "./pages/Login/Login";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
