9ea034cf0359e661891e27ee800a68b4d00970e4
Pedro Lucas <izielucas2016@gmail.com>
2024-07-20 11:25:10 -0300

    started home

--- src/pages/Home/Components/footer.jsx
+++ src/pages/Home/Components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
-        <footer className="text-center text-lg-start footer-login">
-            <p className="text-center p-3 fw-bold font-weight-bold text-white text-uppercase">todos os direitos reservados &copy;</p>
+        <footer className="text-center text-lg-start footer-login align-self-end">
+            <p className="text-center p-3 fw-bold font-weight-bold text-white text-uppercase ">todos os direitos reservados &copy;</p>
         </footer>
     )
 };
