8ef9eb2d363ebb8d6c07cee68a91d143c86c9613
lucarl07 <junitinho2007@gmail.com>
2024-07-22 11:19:15 -0300

    :recycle: refactor: Classes in the Calendar component

--- src/pages/App/Calendar/Calendar.jsx
+++ src/pages/App/Calendar/Calendar.jsx
@@ -4,12 +4,13 @@ import ArrowPng from "../../../assets/images/left-facing-arrow.png";
 
 const Calendar = () => {
   return (
-    <section className="container calendar">
-      <header className="header">
+    <section className="container calendar border-0 rounded-4">
+      <header className="header d-flex px-4 py-4 border-0 rounded-top-4">
         <img className="lf" src={ArrowPng} alt="Seta para ver o mês anterior" />
         <h1 className="title">
-          Mês de {" "}
-          <span>Ano</span>
+          <span className="month">Mês</span> 
+          {" "} de {" "}
+          <span className="year">Ano</span>
         </h1>
         <img className="rf" src={ArrowPng} alt="Seta para ver o próximo mês" />
       </header>
