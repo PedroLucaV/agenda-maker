63322a55479ab5803542a18cc06d91a97664a72c
lucarl07 <junitinho2007@gmail.com>
2024-07-22 12:09:44 -0300

    :sparkles: feat: Implement FullCalendar in src\pages\App\Calendar

--- src/pages/App/Calendar/Calendar.jsx
+++ src/pages/App/Calendar/Calendar.jsx
@@ -1,19 +1,24 @@
-import "./Calendar.css";
+// Dependencies:
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
+// Resources:
 import ArrowPng from "../../../assets/images/left-facing-arrow.png";
+
+// Stylesheets:
+import "./Calendar.css";
 
 const Calendar = () => {
   return (
-    <section className="container calendar border-0 rounded-4">
-      <header className="header d-flex px-4 py-4 border-0 rounded-top-4">
-        <img className="lf" src={ArrowPng} alt="Seta para ver o mês anterior" />
-        <h1 className="title">
-          <span className="month">Mês</span> 
-          {" "} de {" "}
-          <span className="year">Ano</span>
-        </h1>
-        <img className="rf" src={ArrowPng} alt="Seta para ver o próximo mês" />
-      </header>
+    <section className="container calendar p-4 h-100 border-0 rounded-4">
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+        events={[
+          { title: 'Olá mundo!', date: '2024-07-22' },
+          { title: 'Aniversário da Empresa', date: '2024-07-02' }
+        ]}
+      />
     </section>
   );
 };
