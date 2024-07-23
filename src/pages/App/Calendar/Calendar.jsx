22b87659390a61888706db7ca766da5078444ef1
lucarl07 <junitinho2007@gmail.com>
2024-07-23 18:28:44 -0300

    :broom: cleanup: Unused imports at src\pages\App\Calendar

--- src/pages/App/Calendar/Calendar.jsx
+++ src/pages/App/Calendar/Calendar.jsx
@@ -2,9 +2,6 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
-// Resources:
-import ArrowPng from "../../../assets/images/left-facing-arrow.png";
-
 // Stylesheets:
 import "./Calendar.css";
 
@@ -12,17 +9,34 @@ const onEventClick = (info) => {
   alert(`Um evento foi clicado! \nNome: ${info.title}`);
 };
 
+const onDateClick = (info) => {
+  alert(`Uma data foi clicada! \nNome: ${info}`);
+};
+
 const Calendar = () => {
   return (
     <section className="container calendar p-4 h-100 border-0 rounded-4">
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, multiMonthPlugin]}
+        themeSystem="bootstrap5"
         initialView="dayGridMonth"
         locale={"pt-br"}
+        headerToolbar={{
+          start: 'prev', 
+          center: 'title',
+          end: 'next'
+        }}
+        footerToolbar={{
+          start: 'today',
+          end: 'dayGridMonth,multiMonthYear'
+        }}
         buttonText={{
           today: "Ir para hoje",
+          dayGridMonth: "Mês",
+          multiMonthYear: "Ano"
         }}
         eventClick={({ event }) => onEventClick(event)}
+        dateClick={({ dateStr }) => onDateClick(dateStr)}
         events={[
           { title: "Olá mundo!", date: "2024-07-22" },
           { title: "Aniversário da Empresa", date: "2024-07-02" },
