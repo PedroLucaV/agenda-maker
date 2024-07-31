5e555f4e1b46e100bb9f697aa1c89970119ac3fb
Pedro Lucas <izielucas2016@gmail.com>
2024-07-31 11:34:59 -0300

    fixed error on deploy

--- routes/appoitmentsRoutes.js
+++ routes/appoitmentsRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { alterInfoAppointment, createAppointments, listAppointments, deleteAppointment, getByMonth } from "../controller/appointmentsControler.js";
+import { alterInfoAppointment, createAppointments, listAppointments, getByMonth } from "../controller/appointmentsControler.js";
 const router = Router();
 
 router.get('/agendamentos', listAppointments);
@@ -7,8 +7,6 @@ router.get('/agendamentos', listAppointments);
 router.post('/agendamentos', createAppointments);
 
 router.put('/agendamento/:id', alterInfoAppointment);
-
-router.delete('/agendamento/:id', deleteAppointment);
 
 router.get('/agendamento/:month', getByMonth);
 
