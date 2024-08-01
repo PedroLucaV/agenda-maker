990a2743ddf29f1725c9716f8541d3f01f7023e8
lucarl07 <junitinho2007@gmail.com>
2024-07-31 23:41:25 -0300

    🚧 WIP: createEvent module

--- src/helpers/create-event-tester.js
+++ src/helpers/create-event-tester.js
@@ -1,5 +1,31 @@
 const createEvent = () => {
-  window.alert('Olá mundo!')    
+  let eventName, start, end;
+  
+  do {
+    eventName = prompt("Digite o nome do evento: ")
+    if (!eventName) {
+      let confirmWindow = confirm("Por favor, digite o nome do evento.")
+      if (confirmWindow === false) {return}
+    }
+  } while (!eventName)
+
+  do {
+    start = prompt("Digite a data/hora de início: ")
+    if (!start) {
+      let confirmWindow = confirm("Por favor, digite no mínimo a data de início.")
+      if (confirmWindow === false) {return}
+    }
+  } while (!start)
+
+  do {
+    end = prompt("Digite a data/hora de término: ")
+    if (!end) {
+      let confirmWindow = confirm("Por favor, digite no mínimo a data de término.")
+      if (confirmWindow === false) {return}
+    }
+  } while (!end)
+
+  alert('Opa!')
 }
 
 export default createEvent;
