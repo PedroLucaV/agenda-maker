aaf95d3b4073e398098de8e2ff2768363141e1ea
lucarl07 <junitinho2007@gmail.com>
2024-08-01 19:06:50 -0300

    💥 fix: Revert changes at src\helpers\onDateClick.js
    The function's event creation feature was removed, to be transposed to another React component.

--- src/helpers/onDateClick.js
+++ src/helpers/onDateClick.js
@@ -1,27 +1,7 @@
-const onDateClick = (info, calendarRef) => {  
-  const confQuery = confirm(`A data ${info.dateStr} foi clicada. Deseja fazer um agendamento nesse espaço de tempo?`)
-  console.log(calendarRef)
+const onDateClick = (date) => {  
+  alert(`Uma data foi clicada! \nNome: ${date.dateStr}`);
 
-  if (confQuery) {
-    let eventTitle;
-
-    do {
-      eventTitle = prompt("Digite o nome do evento: ")
-      if (!eventTitle) {
-        let confirmWindow = confirm("Deseja continuar com a criação do evento?")
-        if (confirmWindow === false) {return}
-      }
-    } while (!eventTitle)
-
-    const api = calendarRef.current.getApi()
-
-    api.addEvent({
-      id: 1,
-      title: eventTitle,
-      date: info.dateStr,
-      allDay: info.allDay
-    })
-  }
+  console.log(`${date.dateStr} - Dia livre para criar eventos.`)
 };
 
 export default onDateClick;
