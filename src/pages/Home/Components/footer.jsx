4f4beb5bd0ad0ebd3b8ab48f7e5bfdee715ca1ba
Pedro Lucas <izielucas2016@gmail.com>
2024-08-02 11:12:35 -0300

    footer fixed

--- src/pages/Home/Components/footer.jsx
+++ src/pages/Home/Components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <footer className="text-center text-lg-start footer-login align-self-end">
+        <footer className="text-center footer text-lg-start footer-login align-self-end">
             <p className="text-center p-3 fw-bold font-weight-bold text-white text-uppercase ">todos os direitos reservados &copy;</p>
         </footer>
     )
