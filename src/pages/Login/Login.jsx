bcf075b9d01fc9f157e68a3d35f10df4dafe5899
lucarl07 <junitinho2007@gmail.com>
2024-08-12 11:25:28 -0300

    Quick fix

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
-import LoginForm from '../../components/LoginForm/LoginForm';
-import SignupForm from '../../components/SignUpForm/SignUpForm';
-import './Login.css';
+import { useState } from "react";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import SignupForm from "../../components/SignUpForm/SignUpForm";
+import "./Login.css";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true); // Estado para alternar entre login e cadastro
   const [errors, setErrors] = useState({}); // Estado para erros
 
   return (
-    <main className='page-body'>
+    <main className="page-body">
       <div className="container">
-        <h2 id='container-title' className='fs-1 fw-bold'>{isLogin ? 'Login' : 'Crie uma Conta'}</h2>
-        {errors.apiError && <div className="alert alert-danger">{errors.apiError}</div>}
+        <h2 id="container-title" className="fs-1 fw-bold">
+          {isLogin ? "Login" : "Crie uma Conta"}
+        </h2>
+        {errors.apiError && (
+          <div className="alert alert-danger">{errors.apiError}</div>
+        )}
         {isLogin ? (
           <LoginForm setErrors={setErrors} />
         ) : (
@@ -21,7 +25,9 @@ const Login = () => {
           className="btn btn-secondary mt-3"
           onClick={() => setIsLogin(!isLogin)} // Alternar entre login e cadastro
         >
-          {isLogin ? 'Não tem uma conta? Cadastre-se' : 'Já tem uma conta? Faça login'}
+          {isLogin
+            ? "Não tem uma conta? Cadastre-se"
+            : "Já tem uma conta? Faça login"}
         </button>
       </div>
     </main>
