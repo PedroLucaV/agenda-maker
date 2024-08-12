047380480adafbcff602372eee514f0ac84cfabd
lucarl07 <junitinho2007@gmail.com>
2024-08-11 21:07:23 -0300

    🐛 fix: Wrong file type for src\utils\contextMenuItems

--- src/utils/contextMenuItems.jsx
+++ src/utils/contextMenuItems.jsx
@@ -1,7 +1,6 @@
 import { FaCopy, FaPaste, FaPlus, FaEye, FaTrash, FaEllipsisH, FaCalendar, FaCheckSquare } from 'react-icons/fa';
 
-export let global = [
-  { label: 'Copiar texto ou imagem', action: () => alert('Copiar texto ou imagem clicada'), icon: <FaCopy /> },
+export let global = [{ label: 'Copiar texto ou imagem', action: () => alert('Copiar texto ou imagem clicada'), icon: <FaCopy /> },
   { label: 'Colar texto', action: () => alert('Colar texto clicada'), icon: <FaPaste /> },
   { label: 'Criar Evento...', action: () => alert('Criar Evento clicada'), icon: <FaPlus /> },
   { label: 'Ver mais sobre o evento', action: () => alert('Ver mais sobre o evento clicada'), icon: <FaEye /> },
